b1e496efda68af288e27c91fa7db12c175c7029a
miotti98 <lmiotti98@gmail.com>
2024-09-17 15:33:48 -0300

    Product

--- src/main/java/com/educandoweb/course/services/CategoryService.java
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -21,7 +21,7 @@ public class CategoryService {
 	
 	
 	public Category findById(Long id) {
-		Optional<Category> obj =repository.findById(id);
+		Optional<Category> obj = repository.findById(id);
 		return obj.get();
 		
 	}
